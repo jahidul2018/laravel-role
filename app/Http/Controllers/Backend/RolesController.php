474e6d9b4f2eb588715aeaad96fa31fe7c6b5a6c
jahidul2018 <jahidul@daffodil.ac>
2020-09-17 01:16:59 +0600

    add permission by group and seed

--- app/Http/Controllers/Backend/RolesController.php
+++ app/Http/Controllers/Backend/RolesController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use App\User;
 
 class RolesController extends Controller
 {
@@ -30,7 +31,8 @@ class RolesController extends Controller
     public function create()
     {
         $permissions = Permission::all();
-        return view('backend.pages.roles.create', compact('permissions'));
+        $permission_groups = User::getpermissionGroups();
+        return view('backend.pages.roles.create', compact('permissions', 'permission_groups'));
     }
 
     /**
